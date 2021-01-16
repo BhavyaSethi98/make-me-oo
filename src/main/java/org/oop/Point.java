168725505f8b7a9fd0fb0f53a1f08f27599d9df2
bhavya <sethibhavya1998@gmail.com>
2021-01-16 22:22:58 +0530

    Fix problem of shivam's OOP sulution

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,27 @@
 package org.oop;
 
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
-    public double getX() {
-        return x;
+    public static double distance(Point from, Point to) {
+        double xDistance = getDistance(to.x, from.x);
+        double yDistance = getDistance(to.y, from.y);
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
+    private static double getDistance(double point1, double point2) {
+        return point1 - point2;
     }
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public static double direction(Point from, Point to) {
+        double xDistance = getDistance(to.x, from.x);
+        double yDistance = getDistance(to.y, from.y);
+        return Math.atan2(yDistance, xDistance);
     }
 }
